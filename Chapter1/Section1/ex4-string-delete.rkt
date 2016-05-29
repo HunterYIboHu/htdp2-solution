7e4fd0d29a20eeb1a442ce64e8e73c824158cdc0
Yibo Hu <Yibo Hu>
2016-05-29 21:07:39 +0800

    删除字符串中指定索引位置的字符的函数

--- Chapter1/Section1/ex4-string-delete.rkt
+++ Chapter1/Section1/ex4-string-delete.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname string-delete) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex4-string-delete) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ; 常量
 (define str "helloworld")
 (define i 5)
@@ -13,5 +13,3 @@
 ; 这里需要给函数做一个检查，两个类型检查和一个长度检查
 ; 运行
 (string-delete str i)
-
-\nwarrow
